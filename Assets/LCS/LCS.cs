ba5186b6ad6e77a43e2bf24f33eb1f4473d51d98
Gustavo Santos <gustappsantos@gmail.com>
2022-06-01 20:37:40 -0300

    WIP Improvements

--- Assets/LCS/LCS.cs
+++ Assets/LCS/LCS.cs
@@ -4,44 +4,60 @@ using System.Collections.Generic;
 
 public static class LCS
 {
-    private static int[,] GenerateScoreMatrix(string a, string b)
+    private static int[,] GenerateScoreMatrix2(string a, string b)
     {
-        var table = new int[a.Length + 1, b.Length + 1];
+        var width = a.Length + 1;
+        var height = b.Length + 1;
+        var matrix = new int[width, height];
 
-        for (int r = 1; r < table.GetLength(0); r++)
+        for (int row = 1; row < height; row++)
         {
-            for (int c = 1; c < table.GetLength(1); c++)
+            for (int column = 1; column < width; column++)
             {
-                table[r, c] = a[r - 1] == b[c - 1]
-                    ? table[r - 1, c - 1] + 1
-                    : Math.Max(table[r - 1, c], table[r, c - 1]);
+                matrix[column, row] = CalculateCellScore(a, b, column, row, matrix);
             }
         }
 
-        return table;
+        return matrix;
     }
-    
+
+    private static int CalculateCellScore(string a, string b, int column, int row, int[,] matrix)
+    {
+        var rowChar = b[row - 1];
+        var columnChar = a[column - 1];
+
+        if (rowChar == columnChar)
+        {
+            var northWestScore = matrix[column - 1, row - 1];
+            return northWestScore + 1;
+        }
+
+        var westScore = matrix[column - 1, row];
+        var northScore = matrix[column, row - 1];
+        return Math.Max(westScore, northScore);
+    }
+
     private static string TracebackPathWithHighestScore(string a, int[,] scoreMatrix)
     {
         var result = new LinkedList<char>();
-        var rr = scoreMatrix.GetLength(0) - 1;
-        var cc = scoreMatrix.GetLength(1) - 1;
+        var row = scoreMatrix.GetLength(0) - 1;
+        var column = scoreMatrix.GetLength(1) - 1;
 
-        while (scoreMatrix[rr, cc] != 0)
+        while (scoreMatrix[row, column] != 0)
         {
-            if (scoreMatrix[rr, cc - 1] == scoreMatrix[rr, cc])
+            if (scoreMatrix[row, column - 1] == scoreMatrix[row, column])
             {
-                cc -= 1;
+                column -= 1;
             }
-            else if (scoreMatrix[rr - 1, cc] == scoreMatrix[rr, cc])
+            else if (scoreMatrix[row - 1, column] == scoreMatrix[row, column])
             {
-                rr -= 1;
+                row -= 1;
             }
             else
             {
-                result.AddFirst(a[rr - 1]);
-                rr -= 1;
-                cc -= 1;
+                result.AddFirst(a[row - 1]);
+                row -= 1;
+                column -= 1;
             }
         }
 
@@ -50,7 +66,8 @@ public static class LCS
 
     public static string Find(string a, string b)
     {
-        var scoreMatrix = GenerateScoreMatrix(a, b);
-        return TracebackPathWithHighestScore(a, scoreMatrix);;
+        var scoreMatrix = GenerateScoreMatrix2(a, b);
+        return TracebackPathWithHighestScore(a, scoreMatrix);
+        ;
     }
 }
